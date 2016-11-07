aa2db212fa60d75689c41a74313f73818910f42a
haixu <haixu777@gmail.com>
2016-11-07 09:51:13 +0800

    修改sql语句

--- models/sqlMapping.js
+++ models/sqlMapping.js
@@ -1,8 +1,8 @@
 var sql = {
     events: 'SELECT * FROM t_subject WHERE first_name regexp?',
-    sinaAccount: 'SELECT * FROM sinaAccount WHERE screenName regexp?',
+    sinaAccount: 'SELECT accountId,screenName,description FROM sinaAccount WHERE screenName regexp?',
     events_top10: 'SELECT * FROM t_subject order by rand() limit 10',
-    sinaAccount_top10: 'select max(followersCount) from sinaAccount limit 10'
+    sinaAccount_top10: 'select accountId,screenName,followersCount FROM sinaAccount order by followersCount desc limit 10'
 }
 
 module.exports = sql;
