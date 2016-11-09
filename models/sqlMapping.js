e657a28abd1b1ebc1d926f1cf17d124db9534bfa
haixu <haixu777@gmail.com>
2016-11-09 14:17:02 +0800

    修改获取event_top10的sql语句

--- models/sqlMapping.js
+++ models/sqlMapping.js
@@ -2,7 +2,7 @@ var sql = {
     index: {
         events: 'SELECT * FROM t_subject WHERE first_name regexp?',
         sinaAccount: 'SELECT accountId,screenName,description FROM sinaAccount WHERE screenName regexp?',
-        events_top10: 'SELECT * FROM t_subject ORDER BY rand() LIMIT 10',
+        events_top10: 'SELECT id,first_name FROM t_subject ORDER BY id DESC LIMIT 10',
         sinaAccount_top10: 'SELECT accountId,screenName,followersCount FROM sinaAccount ORDER BY followersCount DESC LIMIT 10'
     },
     singleChat: {
