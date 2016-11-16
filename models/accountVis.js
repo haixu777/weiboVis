94af76cc7d828dbb3cb3e20a779396263775db04
yezii <haixu777@gmail.com>
2016-11-16 16:31:28 +0800

    添加wordCloud里的value值,By 近朱

--- models/accountVis.js
+++ models/accountVis.js
@@ -115,7 +115,7 @@ module.exports = {
                     var weibolist =[];
                     var nodes = [];
                     for(var i = 0; i < results.length; i++){
-                        wordcloud[i] = {name: results[i].sample_keyword, id: results[i].keywordId, value: null};
+                        wordcloud[i] = {name: results[i].sample_keyword, id: results[i].keywordId, value: results[i].wval};
                         weibolist[i] = {name: results[i].content, id: results[i].weiboId};
                         nodes[i] = {}
                     }
