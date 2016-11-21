39bec6fc4fb25af974af550cb11a8844a49534bf
yezii <haixu777@gmail.com>
2016-11-21 12:44:09 +0800

    修改.gitinore，修复跨域请求问题

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({origin: 'http://192.168.255.206:3000'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -43,6 +45,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
 
 // error handlers
 
